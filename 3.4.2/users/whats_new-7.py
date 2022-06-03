5480f939390f55805871a758bb37bb0376bfc200
zjzn222 <106813703+zjzn222@users.noreply.github.com>
2022-06-03 19:41:15 +0800

    Delete 3.4.2 directory

--- 3.4.2/users/whats_new-7.py
+++
@@ -1,8 +0,0 @@
-data = {
-    'a': np.random.rand(1000),
-    'b': np.random.rand(1000),
-    'c': np.random.rand(1000),
-}
-
-fig, ax = plt.subplots()
-ax.hexbin('a', 'b', C='c', data=data, gridsize=10)
